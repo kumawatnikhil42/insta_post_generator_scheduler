c6f48c4daa00f647b372c92173a9f4d34ae6adc4
Nikhil Kumawat <143185335+kumawatnikhil42@users.noreply.github.com>
2025-08-06 18:55:37 +0530

    Update post_to_instagram.py

--- post_to_instagram.py
+++ post_to_instagram.py
@@ -7,5 +7,12 @@ load_dotenv()
 
 def post_image(image_path, caption):
     cl = Client()
+
+    # Load session from GitHub secret
+    session_data = os.getenv("IG_SESSION")
+    if not session_data:
+        raise ValueError("❌ IG_SESSION secret is missing.")
+
+    cl.load_settings(json.loads(session_data))
     cl.login(IG_USER, IG_PASS)
     cl.photo_upload(image_path, caption)
