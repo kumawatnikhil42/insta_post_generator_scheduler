32e88a80be6773a21121215689c5667f4a7bc09c
Nikhil Kumawat <143185335+kumawatnikhil42@users.noreply.github.com>
2025-08-06 18:05:48 +0530

    Update post_to_instagram.py

--- post_to_instagram.py
+++ post_to_instagram.py
@@ -1,10 +1,11 @@
 from instagrapi import Client
 import os
 from dotenv import load_dotenv
+from config import IG_USER,IG_PASS
 
 load_dotenv()
 
 def post_image(image_path, caption):
     cl = Client()
-    cl.login(os.getenv("IG_USER"), os.getenv("IG_PASS"))
+    cl.login(IG_USER, IG_PASS)
     cl.photo_upload(image_path, caption)
